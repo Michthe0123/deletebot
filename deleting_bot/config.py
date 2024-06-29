cf0529c2ca1738717f7b14b4a2ac9243faccd082
Michthe0123 <abhinavkrishna101@gmail.com>
2024-06-29 18:06:15 +0530

    Update config.py

--- deleting_bot/config.py
+++ deleting_bot/config.py
@@ -1,5 +1,5 @@
 # config.py
 BOT_TOKEN = '7159655114:AAGq40wkATP7fBhBtNcBxPpx-ntv4wGsajo'
-ALLOWED_GROUP_IDS = [-1001876110191]  # Replace with your actual group IDs
+ALLOWED_GROUP_IDS = [-1001423451813,-1001491222339]  # Replace with your actual group IDs
 # Time delay in seconds before deleting messages
 DELETE_DELAY = 10  # Adjust the value as needed
